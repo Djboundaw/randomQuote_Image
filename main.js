76d8e40d103e11435e91336730d5103f2614c48d
Djibril NDAW <78438426+Djboundaw@users.noreply.github.com>
2022-02-20 12:27:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -8,25 +8,12 @@ const getQuote = () => {
       quote.innerHTML = `« ${data.content} » <br/> <br/> by ${data.author}`;
     });
 
-  // fetch('https://picsum.photos/600/900')
-  //     .then((res) => {
-  //         document.getElementById('pic').innerHTML = `<img src="${res.url}"/>`;
-  //         // let background = res.url;
-  //         // console.log(background);
-  //         // quote.style.background = `url(${res.url})`;
-  //     })
-  //     // .then((data) => {
-  //     //     console.log(data.content);
-  //     //     quote.style.background = ""
-  //     // })
-
   let num = nombreAleatoire();
   // console.log(num);
   // document.getElementById('pic').innerHTML = `<img src="img/${num}.jpg" sty/>`;
   document.getElementById("pic").backgroundImage = `url('${num}.jpg')`;
 };
 
-/*----------------- Choix nombre aléatoire -----------------*/
 function nombreAleatoire() {
   return Math.floor(Math.random() * 50 + 1);
 }
